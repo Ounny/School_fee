8c1527bb7d792ee96c7c26d999fdcb2db0e94157
Sokhour10 <121353277+Sokhour10@users.noreply.github.com>
2023-11-21 13:54:46 +0700

    Update schoolpaymentReminder.java

--- School Fees reminder/Javafees/schoolpaymentReminder.java
+++ School Fees reminder/Javafees/schoolpaymentReminder.java
@@ -1,5 +1,30 @@
 package Javafees;
 
-public class schoolpaymentReminder {
-    
+public class schoolpaymentReminder 
+{
+    private String name;
+    private String paymentRecord;
+    // Constructor
+    public schoolpaymentReminder(String name, String paymentrecord)
+    {
+        this.name=name;
+        this.paymentRecord=paymentrecord;
+    }
+    //Getter and Setter
+    public String getname()
+    {
+        return this.name;
+    }
+    public void setname(String name)
+    {
+        this.name=name;
+    }
+     public String getpaymentrecord()
+    {
+        return this.paymentRecord;
+    }
+    public void setpaymentrecord(String paymentRecord)
+    {
+        this.paymentRecord=paymentRecord;
+    }
 }
